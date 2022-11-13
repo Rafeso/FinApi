75cc638f4aa870a8bb65a6f83cef291119383a12
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-11-13 08:00:05 -0300

    Commit: Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
 	return next();
 }
 
+function getBalance(statement) {
+	const balance = statement.reduce((acc, operation) => {
+		if (operation.type === "credit") {
+			return acc + operation.amount;
+		} else {
+			return acc - operation.amount;
+		}
+	}, 0);
+
+	return balance;
+}
+
 app.post("/account", (request, response) => {
 	const { cpf, name } = request.body;
 
@@ -51,7 +63,6 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
 
 app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
 	const { description, amount } = request.body;
-
 	const { customer } = request;
 
 	const statementOperation = {
@@ -63,7 +74,28 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
 
 	customer.statement.push(statementOperation);
 
-  return response.status(201).send();
+	return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+	const { amount } = request.body;
+	const { customer } = request;
+
+	const balance = getBalance(customer.statement);
+
+	if (balance < amount) {
+		return response.status(400).json({ error: "insufficient funds!" });
+	}
+
+	const statementOperation = {
+		amount,
+		createdAt: new Date(),
+		type: "debit",
+	};
+
+	customer.statement.push(statementOperation);
+
+	return response.status(201).send();
 });
 
 app.listen(3333);
